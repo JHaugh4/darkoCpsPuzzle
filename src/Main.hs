0f69f3bbea6f5bdd7df76087710c1b30b6cf9680
Joe <jchaugh@salud.unm.edu>
2020-03-02 11:12:32 -0700

    Adde comments.

--- src/Main.hs
+++ src/Main.hs
@@ -59,10 +59,26 @@ toCPS_DanvyFilinski_HigherOrder_US t = (fst (runState (cc t) 1))
         ts' <- traverse cc ts
         return $ answer (zip ls ts')
 
+{-
+  Accepts the list of records where the term has already been cpsd.
+  It then takes off the first record and generates a lambda with the kappa
+  continuation that will be used in the base case of answer'. It then uses
+  the cpsd term to collapse the call to answer'. The call to answer' is given
+  its final continuation the rest of the cpsd records the initial label and empty
+  list to collect the values.
+-}
 answer :: [(Label, (Term -> Term) -> Term)] -> ((Term -> Term) -> Term)
-answer []          = error "Not good"
+answer []           = error "Not good"
 answer ((l, t):lts) = \kappa -> t (answer' kappa lts [l] [])
 
+{-
+  This function does the tricky job of seemingly creating nested lambdas at runtime.
+  The trick is to call cpsd term t on the recursive call. Notice that I do not give the last
+  term argument to the recursive call thus it has type Term -> Term which is what t needs to return
+  a term. Once I saw that trick the rest was relatively simple, loop through the records and add their
+  return values v and labels l to list as you go along. Then when you reach the end of the records
+  use the continuation kappa to do the final collapse and hand the labels and values to Record.
+-}
 answer' :: (Term -> Term) -> [(Label, (Term -> Term) -> Term)] -> [Label] -> [Term] -> Term -> Term
 answer' kappa [] ls vs           = \v -> kappa (Record $ zip ls (vs ++ [v]))
 answer' kappa ((l, t):lts) ls vs = \v -> t (answer' kappa lts (ls ++ [l]) (vs ++ [v]))
