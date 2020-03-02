fcc0f4be60949d2cb93a4fd5038699d1b8e5c1ba
Joe <jchaugh@salud.unm.edu>
2020-03-02 11:06:07 -0700

    Seems to work...

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,8 @@ import Control.Monad.State
 
 type Label  = String
 type Var    = String
-data Const  = Tru | Fls
-data PrimOp = Add | Sub | Mul
+data Const  = Tru | Fls | IntConst Integer | CharConst Char | Unit deriving (Eq, Show)
+data PrimOp = Add | Sub | Mul deriving (Eq, Show)
 
 data Term = Var Var
           | Abs Var Term
@@ -13,6 +13,7 @@ data Term = Var Var
           | Const Const
           | PrimApp PrimOp [Term]
           | Record [(Label, Term)]
+          deriving (Eq, Show)
 
 toCPS_DanvyFilinski_HigherOrder_US :: Term -> (Term -> Term) -> Term
 toCPS_DanvyFilinski_HigherOrder_US t = (fst (runState (cc t) 1))
@@ -52,18 +53,31 @@ toCPS_DanvyFilinski_HigherOrder_US t = (fst (runState (cc t) 1))
         return (\kappa -> t1'
                  (\v1 -> t2'
                    (\v2 -> kappa (PrimApp p [v1, v2]))))
+      Record rs -> do
+        let ls = fmap fst rs
+        let ts = fmap snd rs
+        ts' <- traverse cc ts
+        return $ answer (zip ls ts')
 
-answer :: [(Term -> Term) -> Term] -> ((Term -> Term) -> Term)
-answer []     = error "Not good"
-answer (t:ts) = \kappa -> t (answer' kappa ts [])
+answer :: [(Label, (Term -> Term) -> Term)] -> ((Term -> Term) -> Term)
+answer []          = error "Not good"
+answer ((l, t):lts) = \kappa -> t (answer' kappa lts [l] [])
 
-freshLabels :: [Label]
-freshLabels = fmap show [ 0.. ]
+answer' :: (Term -> Term) -> [(Label, (Term -> Term) -> Term)] -> [Label] -> [Term] -> Term -> Term
+answer' kappa [] ls vs           = \v -> kappa (Record $ zip ls (vs ++ [v]))
+answer' kappa ((l, t):lts) ls vs = \v -> t (answer' kappa lts (ls ++ [l]) (vs ++ [v]))
 
-answer' :: (Term -> Term) -> [(Term -> Term) -> Term] -> [Term] -> Term -> Term
-answer' kappa [] vs     = \v -> kappa (Record $ zip freshLabels (vs ++ [v]))
-answer' kappa (t:ts) vs = \v -> t (answer' kappa ts (vs ++ [v]))
-  
+testTerm1 :: Term
+testTerm1 = PrimApp Add [ Const $ IntConst 1, Const $ IntConst 2 ]
+
+testTerm2 :: Term
+testTerm2 = PrimApp Add [ App (Abs "x" (Const $ IntConst 1)) (Const Tru), Const $ IntConst 2 ]
+
+testTerm3 :: Term
+testTerm3 = Record [("1", App (Abs "x" (Const $ IntConst 1)) (Const Tru)), ("2", Const $ IntConst 2), ("3", testTerm2)]
+
+--answer' kappa [] vs     = \v -> kappa (PrimApp Add (vs ++ [v]))
+
 main :: IO ()
 main = do
   putStrLn "hello world"
